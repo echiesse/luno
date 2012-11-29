d8202d4908321405c15f40756d9ef8e024c86e39
Eric Chiesse <echiesse@gmail.com>
2012-11-29 00:28:21 -0300

    Criadas as seguintes funções:     exposeAll  - Chama exposeAll em todos os módulos que a tenham definido     exposeSome - Chama exposeSome em todos os módulos que a tenham definido     useAliases - Chama useAlias em todos os módulos que a tenham definido

--- src/base.lua
+++ src/base.lua
@@ -29,3 +29,37 @@ end
 
 
 lunoStartup()
+
+--##############################################################################
+function luno.exposeAll()
+    for k, v in pairs(luno) do
+        if type(v) == "table" then
+            local libExposeAll = v["exposeAll"]
+            if type(libExposeAll) == "function" then
+                libExposeAll()
+            end
+        end
+    end
+end
+
+function luno.exposeSome()
+    for k, v in pairs(luno) do
+        if type(v) == "table" then
+            local libExposeAll = v["exposeSome"]
+            if type(libExposeSome) == "function" then
+                libExposeSome()
+            end
+        end
+    end
+end
+
+function luno.useAliases()
+    for k, v in pairs(luno) do
+        if type(v) == "table" then
+            local useAlias = v["useAlias"]
+            if type(useAlias) == "function" then
+                useAlias()
+            end
+        end
+    end
+end
